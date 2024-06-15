213fcf12625d03c94766814c8f284586bb0a4a13
Tomáš Diblík <dibla.tomas@post.cz>
2024-06-15 20:52:15 +0200

    [Feature] Works

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 )
@@ -12,4 +18,108 @@ func main() {
 		log.Fatal("Error loading .env file")
 		return
 	}
+
+	GH_TOKEN := os.Getenv("GH_TOKEN")
+	if len(GH_TOKEN) == 0 {
+		log.Fatal("GH_TOKEN cannot be empty")
+		return
+	}
+	MIRROR_PATH := os.Getenv("MIRROR_PATH")
+	if len(MIRROR_PATH) == 0 {
+		log.Fatal("MIRROR_PATH cannot be empty")
+		return
+	}
+
+	req, err := http.NewRequest("GET", "https://api.github.com/user/repos", nil)
+	if err != nil {
+		log.Fatalf("error creating /user/repos http request: %s\n", err)
+		os.Exit(1)
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+GH_TOKEN)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("error calling /user/repos http request: %s\n", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("error reading /user/repos http body: %s\n", err)
+		os.Exit(1)
+	}
+
+	var repos []Repository
+	err = json.Unmarshal(data, &repos)
+	if err != nil {
+		log.Fatalf("error unmarshalling /user/repos JSON data: %s\n", err)
+		os.Exit(1)
+	}
+	log.Println("Successfuly called and parsed /user/repos")
+
+	for _, repo := range repos {
+		log.Println("running for " + repo.FullName)
+		command := "cd \"" + MIRROR_PATH + "\"; "
+
+		owner_directory := filepath.Join(MIRROR_PATH, repo.Owner.Login)
+		owner_directory_exists, err := doesPathExists(owner_directory)
+		if err != nil {
+			log.Fatalf("unable to find out whether path (%s) exists: %s", owner_directory, err)
+			os.Exit(1)
+		}
+		if !owner_directory_exists {
+			log.Println("will freshly create: ", repo.Owner.Login)
+			command += "mkdir \"" + repo.Owner.Login + "\"; "
+		}
+		command += "cd \"" + repo.Owner.Login + "\"; "
+
+		repo_directory := filepath.Join(MIRROR_PATH, repo.Name)
+		repo_directory_exists, err := doesPathExists(repo_directory)
+		if err != nil {
+			log.Fatalf("unable to find out whether path (%s) exists: %s", repo_directory, err)
+			os.Exit(1)
+		}
+		if !repo_directory_exists {
+			log.Println("will freshly clone: ", repo.FullName)
+			command += "git clone https://" + GH_TOKEN + "@github.com/" + repo.FullName + ".git; "
+		} else {
+			log.Println("will only mirror changes to: ", repo.FullName)
+		}
+		command += "cd \"" + repo.Name + "\"; "
+
+		command += `
+			base_branch=$(git branch --show-current);
+
+			git fetch --all;
+			for branch in $(git branch -r | grep -v '\->'); do
+				git branch --track "${branch#origin/}" "$branch" || true
+			done
+
+			for branch in $(git for-each-ref --format='%(refname:short)' refs/heads/); do
+				git checkout "$branch"
+				git reset --hard "origin/$branch"
+			done;
+			
+			git checkout "$base_branch"
+		`
+
+		log.Println("started work...")
+		stdout, err := exec.Command("bash", "-c", command).Output()
+		if err != nil {
+			log.Fatalf("Error running the command (%s): %s\n", command, err)
+			os.Exit(1)
+		}
+		log.Println("Result of the commands executed: \n ----------------- \n", string(stdout), "-----------------")
+	}
+}
+
+func doesPathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
